2851778f6a5f7d87c4d848578d11252b7d4730c7
John Donnellan <john@codybartfast.com>
2023-01-28 12:47:35 +0000

    Create new webapp and add pages from CLI

---
+++ Pages/Signin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloGithubOAuth.Pages
+{
+    public class SigninModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
